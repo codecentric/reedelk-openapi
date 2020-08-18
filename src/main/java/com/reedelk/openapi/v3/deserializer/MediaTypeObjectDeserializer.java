ed6c443d03924d18fadb5311aee72e02cb9f3b2f
lorenzo <info@reedelk.com>
2020-08-18 11:22:16 +0200

    feature: working on OpenAPI import feature.

--- src/main/java/com/reedelk/openapi/v3/deserializer/MediaTypeObjectDeserializer.java
+++ src/main/java/com/reedelk/openapi/v3/deserializer/MediaTypeObjectDeserializer.java
@@ -1,6 +1,7 @@
 package com.reedelk.openapi.v3.deserializer;
 
 import com.reedelk.openapi.commons.AbstractDeserializer;
+import com.reedelk.openapi.commons.DataFormat;
 import com.reedelk.openapi.v3.DeserializerContext;
 import com.reedelk.openapi.v3.model.Example;
 import com.reedelk.openapi.v3.model.MediaTypeObject;
@@ -29,8 +30,8 @@ public class MediaTypeObjectDeserializer extends AbstractDeserializer<MediaTypeO
                 mediaTypeObject.setExample(example);
             } else {
                 // If it is an object must have the type of the
-                // OpenAPI document: must be either YAML or JSON.
-                String exampleAsString = context.getDataFormat().dump(exampleData);
+                // OpenAPI document: must be serialized as JSON.
+                String exampleAsString = DataFormat.JSON.dump(exampleData);
                 Example example = new Example(exampleAsString);
                 mediaTypeObject.setExample(example);
             }
