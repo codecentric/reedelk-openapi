109ae1fdb0070b99ee2d095c8bd1de474cd91a8b
lorenzo <info@reedelk.com>
2020-08-26 09:44:00 +0200

    misc: adding unit tests.

--- src/test/java/com/reedelk/openapi/v3/ContactObjectTest.java
+++ src/test/java/com/reedelk/openapi/v3/ContactObjectTest.java
@@ -2,20 +2,37 @@ package com.reedelk.openapi.v3;
 
 import com.reedelk.openapi.Fixture;
 import com.reedelk.openapi.v3.model.ContactObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ContactObjectTest extends AbstractOpenApiSerializableTest {
 
-    @Test
-    void shouldCorrectlySerializeContactWithAllProperties() {
-        // Given
-        ContactObject contact = new ContactObject();
+    private ContactObject contact;
+
+    @BeforeEach
+    void setUp() {
+        contact = new ContactObject();
         contact.setName("API Support");
         contact.setUrl("http://www.example.com/support");
         contact.setEmail("support@example.com");
+    }
+
+    @Test
+    void shouldCorrectlySerializeContactWithAllProperties() {
+        // Given
+        ContactObject theContact = contact;
 
         // Expect
-        assertSerializeJSON(contact, Fixture.ContactObject.WithAllProperties);
+        assertSerializeJSON(theContact, Fixture.ContactObject.WithAllProperties);
+    }
+
+    @Test
+    void shouldCorrectlyDeserializeContactWithAllProperties() {
+        // Given
+        ContactObject theContact = contact;
+
+        // Expect
+        assertDeserializeJSON(theContact, Fixture.ContactObject.WithAllProperties);
     }
 
     @Test
@@ -25,5 +42,14 @@ public class ContactObjectTest extends AbstractOpenApiSerializableTest {
 
         // Expect (expect empty, because there are no required properties for contact)
         assertSerializeJSON(contact, Fixture.ContactObject.WithDefaultProperties);
+    }
+
+    @Test
+    void shouldCorrectlyDeserializeContactWithRequiredValues() {
+        // Given
+        ContactObject contact = new ContactObject();
+
+        // Expect (expect empty, because there are no required properties for contact)
+        assertDeserializeJSON(contact, Fixture.ContactObject.WithDefaultProperties);
     }
 }
